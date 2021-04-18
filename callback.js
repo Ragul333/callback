2483bdfa860d5fb7610b3e6cc6895ccecf5efd39
Ragul333 <63944210+Ragul333@users.noreply.github.com>
2021-04-18 08:10:01 +0530

    Add files via upload

---
+++ callback.js
@@ -0,0 +1,33 @@
+setTimeout(()=>{
+    document.body.innerHTML='<h1>10</h1>';
+    setTimeout(()=>{
+    document.body.innerHTML='<h1>9</h1>';
+    setTimeout(()=>{
+    document.body.innerHTML='<h1>8</h1>';
+    setTimeout(()=>{
+    document.body.innerHTML='<h1>7</h1>';
+    setTimeout(()=>{
+    document.body.innerHTML='<h1>6</h1>';
+    setTimeout(()=>{
+    document.body.innerHTML='<h1>5</h1>';
+    setTimeout(()=>{
+    document.body.innerHTML='<h1>4</h1>';
+    setTimeout(()=>{
+    document.body.innerHTML='<h1>3</h1>';
+    setTimeout(()=>{
+    document.body.innerHTML='<h1>2</h1>';
+    setTimeout(()=>{
+    document.body.innerHTML='<h1>1</h1>';
+    setTimeout(()=>{
+        document.body.innerHTML='<h1>HAPPY INDEPENDENCE DAY</h1>';
+},1000)
+},1000)
+},1000)
+},1000)
+},1000)
+},1000)
+},1000)
+},1000)
+},1000)
+},1000)
+},1000)
